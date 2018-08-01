98a118f8589735cdd436cb3a3c31c7235fa00d36
Rehan Durrani <rdurrani@berkeley.edu>
2018-07-31 17:40:27 -0700

    Add setup.py

--- src/selection_policy/src/setup.py
+++ src/selection_policy/src/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup, Extension
 
 # the c++ extension module
-extension_mod = Extension("rpc", ["rpcmodule.c", "rpc_handle_wrapper.cpp"])
+extension_mod = Extension("rpc", ["rpcmodule.c", "rpc_handle_wrapper.cpp"], include_dirs=['../../libs/concurrent_queue']
+                          , extra_compile_args = ["-stdlib=libc++"])
 
 setup(name = "rpc", ext_modules=[extension_mod])
